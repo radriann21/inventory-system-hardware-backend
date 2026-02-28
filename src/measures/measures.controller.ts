2d8dead82d012273e7389f37a3c1f8a0c806708a
Adrian Rangel <adrianrangelargu@gmail.com>
2026-02-28 12:42:24 -0400

    feat: add JWT authentication guards and HTTP status codes to all protected endpoints

--- src/measures/measures.controller.ts
+++ src/measures/measures.controller.ts
@@ -6,36 +6,50 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { MeasuresService } from './measures.service';
 import { CreateMeasureDto } from './dto/create-measure.dto';
 import { UpdateMeasureDto } from './dto/update-measure.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('measures')
 export class MeasuresController {
   constructor(private readonly measuresService: MeasuresService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createMeasureDto: CreateMeasureDto) {
     return this.measuresService.create(createMeasureDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.measuresService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
     return this.measuresService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
+  @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updateMeasureDto: UpdateMeasureDto) {
     return this.measuresService.update(+id, updateMeasureDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.measuresService.remove(+id);
   }
--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -6,36 +6,49 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
-
+import { AuthGuard } from 'src/auth/auth.guard';
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.createNewProvider(createProviderDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.providersService.findAllProviders();
   }
 
+  @UseGuards(AuthGuard)
   @Get('/name/:name')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('name') name: string) {
     return this.providersService.findOneProviderByName(name);
   }
 
+  @UseGuards(AuthGuard)
   @Get('/id/:id')
+  @HttpCode(HttpStatus.OK)
   findOneById(@Param('id') id: string) {
     return this.providersService.findOneProviderById(id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch('/update/:id')
+  @HttpCode(HttpStatus.OK)
   update(
     @Param('id') id: string,
     @Body() updateProviderDto: UpdateProviderDto,
@@ -43,7 +56,9 @@ export class ProvidersController {
     return this.providersService.updateProvider(id, updateProviderDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete('/delete/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.providersService.deleteProvider(id);
   }
