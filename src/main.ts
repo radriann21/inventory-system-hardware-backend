cf5a8bb9a6231b90289213c65b4e95ebb3d62494
Adrian Rangel <adrianrangelargu@gmail.com>
2026-02-28 12:08:49 -0400

    feat: added global logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
 
   app.use(cookieParser());
 
